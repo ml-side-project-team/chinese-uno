85f828adf20300489fa4ca56616ce8dd606950a7
ItsSynergi <Michael_Leng@outlook.com>
2019-02-02 23:35:48 -0500

    Added .gitignore and minor Model modifications

--- Model.py
+++ Model.py
@@ -20,7 +20,12 @@ class Card:
         self.suit = suit
         self.rank = rank
 
+
 class Player:
+    def __init__(self):
+        self.points = 0
+        self.hand = []
+
     def add_points(self, points):
         self.points = points
 
@@ -28,43 +33,48 @@ class Player:
         self.hand = hand
 
     def play(self):
-        #return which card you want to play
-        #should probably be handled by the controller
+        # return which card you want to play
+        # should probably be handled by the controller
         return Card()
 
+
 def shuffle():
-    #randomly choose a deck
-    Deck = []
-    return Deck
+    # randomly choose a deck
+    deck = []
+    return deck
+
 
 def distribute_cards(players):
-    Deck = shuffle()
-    playerNum = 0
-    playerHands = [[]]
-    for i in range(0, Deck.count()):
-        playerHands[playerNum].append(Deck[i])
-        playerNum = playerNum + 1
-        if playerNum == players.count():
-            playerNum = 0
+    deck = shuffle()
+    player_num = 0
+    player_hands = [[]]
+    for i in range(0, deck.count()):
+        player_hands[player_num].append(deck[i])
+        player_num = player_num + 1
+        if player_num == players.count():
+            player_num = 0
     for j in range(0, players.count()):
-        players[j].set_hand(playerHands[j])
+        players[j].set_hand(player_hands[j])
+
 
 def playable_state():
-    #check to see if at least two players have cards left in their hands
+    # check to see if at least two players have cards left in their hands
     return True
 
+
 def play_round():
-    currentType = "singleCard"
-    currentCardRank = -1
-    #distribute the cards to players
-    #find players with 3 of hearts and choose one
-    #while people can play
-        #cycle through players
-            #each player play their card
-            #assign points for different results (mostly illegal moves)
+    current_type = "singleCard"
+    current_card_rank = -1
+    # distribute the cards to players
+    # find players with 3 of hearts and choose one
+    # while people can play
+        # cycle through players
+            # each player play their card
+            # assign points for different results (mostly illegal moves)
+
 
 def game_loop():
     while playable_state():
         play_round()
-    #assign points to winners
+    # assign points to winners
     return
