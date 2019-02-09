ad1ffec32f0f8c7912c1548b174487e95726c980
Russell Pecka <rpecka98@gmail.com>
2019-02-09 15:41:25 -0500

    Resolved compilation errors and made the game run

--- Model.py
+++ Model.py
@@ -4,12 +4,14 @@ numDecks = 2
 current_type = "singleCard"
 numPlayers = 6
 
+
 class Move:
     Types = ["Pass", "SingleCard"]
 
     def __init__(self, type, cards):
         self.type = type
         self.cards = cards
+
 
 class Card:
     Suits = ["Diamonds", "Clubs", "Hearts", "Spades"]
@@ -41,9 +43,9 @@ class Card:
 
     # returns an ordered deck
     @staticmethod
-    def new_deck(self):
+    def new_deck():
         deck = []
-        for deck in range(0, numDecks):
+        for _ in range(0, numDecks):
             for suit in Card.Suits:
                 for rank in Card.Ranks:
                     deck.append(Card(suit, rank))
@@ -83,19 +85,20 @@ class Player:
 
 
 def shuffle(deck):
-    return random.shuffle(deck)
+    random.shuffle(deck)
 
 
 def distribute_cards(players):
-    deck = shuffle()
+    deck = Card.new_deck()
+    shuffle(deck)
     player_num = 0
-    player_hands = [[]]
-    for i in range(0, deck.count()):
+    player_hands = [[]] * len(players)
+    for i in range(0, len(deck)):
         player_hands[player_num].append(deck[i])
         player_num = player_num + 1
-        if player_num == players.count():
+        if player_num == len(players):
             player_num = 0
-    for j in range(0, players.count()):
+    for j in range(0, len(players)):
         players[j].set_hand(player_hands[j])
 
 
@@ -107,7 +110,7 @@ def playable_state(players):
 def find_first_player(players):
     # find players with 3 of hearts
     competing_players = []
-    for i in range(0, players.size()):
+    for i in range(0, len(players)):
         if Player.has_three_hearts(players[i]):
             competing_players.append(i)
     # choose one at random
@@ -122,13 +125,26 @@ def set_first_player(players, i):
     return players[i:] + players[:i]
 
 
+def move_from_preference(card_play_preference, current_card):
+    if card_play_preference is None:
+        return None
+    for i in range(0, len(card_play_preference)):
+        # check if illegal
+        if Card.is_illegal(current_card, card_play_preference[i]):
+            # TODO dock points
+            return None
+        else:
+            return card_play_preference[i]
+    return None
+
+
 def play_round(players):
-    players_in_round = players.size()
+    players_in_round = len(players)
     current_card = Card("Hearts", "3")
     # find players with 3 of hearts and choose one
-    first_player = find_first_player()
+    first_player = find_first_player(players)
     # make them play (remove card from their hand)
-    players[first_player].removeCard(Card("Hearts", "3"))
+    players[first_player].remove_card(Card("Hearts", "3"))
     # set the player to be the second player (the first must play the 3 of hearts)
     players = set_first_player(players, first_player + 1)
     # TODO this should also check to see if everyone passed
@@ -136,15 +152,12 @@ def play_round(players):
         # cycle through players
         for current_player in range(0, players_in_round):
             # each player play their card
-            card_play_preference = players[current_player].play()
-            for i in range(0, card_play_preference):
-                # check if illegal
-                if Card.is_illegal(current_card, card_play_preference[i]):
-                    # TODO dock points
-                    pass
-                else:
-                    if card_play_preference:
-                        current_card = card_play_preference[i]
+            card_play_preference = players[current_player].play(current_card)
+            move = move_from_preference(card_play_preference, current_card)
+            if move is None:
+                pass
+            else:
+                current_card = move
 
 
 def play_match():
@@ -152,7 +165,10 @@ def play_match():
     for i in range(0, numPlayers):
         players.append(Player())
     distribute_cards(players)
-    while playable_state():
-        play_round()
+    while playable_state(players):
+        play_round(players)
     # TODO assign points to winners
     return
+
+
+play_match()
